92d76c3400ba563f4ff298ba2ddc96dc26517505
Jorge Netto <jorgenetto96@gmail.com>
2023-09-04 19:36:13 -0300

    Password validation using javascript.
    - If password and password confirmation match, then nothing will happen,
    but it they don't match, they will receive an error message.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("testing the script file.")
+let passwordInput = document.querySelector('#password');
+let confirmPasswordInput = document.querySelector('#confirm_pwd');
+let errorMessage = document.querySelector('#error_message');
+
+passwordInput.addEventListener('blur', (e) => {
+    let pwd = passwordInput.value;
+    confirmPasswordInput.addEventListener('blur', () => {
+        let confirm_pwd = confirmPasswordInput.value;
+        
+        if (pwd == confirm_pwd) {
+            errorMessage.innerText = '';
+
+        } else {
+            errorMessage.innerText = 'Passwords don\'t match';
+        }
+    })
+})
