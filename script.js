d706e6b6dd883de3c2c30ceeee140e0df33b9d22
Jorge Netto <jorgenetto96@gmail.com>
2023-09-05 05:20:39 -0300

    Account creation validation: if passwords are ok -> account created.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 let passwordInput = document.querySelector('#password');
 let confirmPasswordInput = document.querySelector('#confirm_pwd');
 let errorMessage = document.querySelector('#error_message');
+let form = document.querySelector('form');
+let passwordsValid = false;
 
-passwordInput.addEventListener('blur', (e) => {
+function validatePasswords() {
     let pwd = passwordInput.value;
-    confirmPasswordInput.addEventListener('blur', () => {
-        let confirm_pwd = confirmPasswordInput.value;
-        
-        if (pwd == confirm_pwd) {
-            errorMessage.innerText = '';
+    let confirm_pwd = confirmPasswordInput.value;
 
-        } else {
-            errorMessage.innerText = 'Passwords don\'t match';
-        }
-    })
+    if (pwd == confirm_pwd) {
+        errorMessage.innerText = '';
+        passwordsValid = true;
+    } else {
+        passwordsValid = false;
+        errorMessage.innerText = 'Passwords don\'t match';
+    }
+}
+
+passwordInput.addEventListener('blur', validatePasswords);
+confirmPasswordInput.addEventListener('blur', validatePasswords);
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (passwordsValid) {
+        alert('you successfully created your account.');
+    } else {
+        alert('your passwords are not valid.')
+    }
 })
